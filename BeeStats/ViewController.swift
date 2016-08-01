436c927c283fedfdec1b0cc08762919c2c9151d9
lukasamueller@icloud.com <millennium1412@yahoo.co.jp>
2016-08-01 15:31:34 -0700

    Initial Commit

---
+++ BeeStats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeeStats
+//
+//  Created by Lukas A. Müller on 01/08/16.
+//  Copyright © 2016 Lukas Mueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
